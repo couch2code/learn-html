3ddfd6d2dfa47fafbb1534ea1fbf3966a324a850
Tom Wilson <tom@jackhq.com>
2016-02-27 15:48:18 -0500

    update add modal

--- ex1/index.js
+++ ex1/index.js
@@ -2,34 +2,27 @@ const h = require('npm:hyperscript')
 const modal = require('github:yuanqing/modal')
 
 module.exports = function () {
+  var d = dialog('Exercise Error!')
   if (document.querySelectorAll('p').length === 3) {
-    const success = dialog('Exercise Success!')
-    document.body.appendChild(success)
-    const m = modal(success)
-    m.show()
-  } else {
-    const error = dialog('Exercise Error!')
-    document.body.appendChild(error)
-    const m1 = modal(error)
-
-    m1.show()
-
+    d = dialog('Exercise Success!')
   }
-}
+  document.body.appendChild(d)
+  const m = modal(d)
+  m.show()
 
-
-function dialog (desc) {
-  return h('.modal', [
-      h('.modal__dialog', {
-          style: {
-              background: 'blue',
-              color: 'white',
-              width: '300px'
-          }
-      }, [
-        h('button'}, ['Ok']),
-        h('h1', [desc])
-      ])
+  function dialog (desc) {
+    return h('.modal', [
+        h('.modal__dialog', {
+            style: {
+                background: 'blue',
+                color: 'white',
+                width: '300px'
+            }
+        }, [
+          h('button', { onclick: _ => m.hide()}, ['Ok']),
+          h('h1', [desc])
+        ])
     ])
+  }
 
 }
